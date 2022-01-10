c241265b4f94792bcdffb8ac291afac14a441f97
chints87 <cgandhi4@gmail.com>
2022-01-10 10:39:22 +0530

    Add firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB-atyHB0i6MdoN85Lnkag2zvt2PwDKb-E',
+  authDomain: 'netflix-clone-fe5ad.firebaseapp.com',
+  projectId: 'netflix-clone-fe5ad',
+  storageBucket: 'netflix-clone-fe5ad.appspot.com',
+  messagingSenderId: '260571828051',
+  appId: '1:260571828051:web:b046d701516273a7d39ba6',
+  measurementId: 'G-XMY5DXVVNZ',
+};
+
+export default firebaseConfig;
