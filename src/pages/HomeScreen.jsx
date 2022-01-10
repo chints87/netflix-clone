64ada6fd65a89dd2d6193b471963d3d3c1e2ce12
chints87 <cgandhi4@gmail.com>
2022-01-10 10:40:08 +0530

    Add Home Screen

---
+++ src/pages/HomeScreen.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styles from '../styles/scss/HomeScreen.module.scss';
+import Nav from '../components/Nav';
+
+export default function HomeScreen() {
+  return (
+    <div className={styles.homeScreen}>
+      <Nav />
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      <h1>Movie</h1>
+      {/* Banner */}
+      {/* Movie Categories */}
+    </div>
+  );
+}
