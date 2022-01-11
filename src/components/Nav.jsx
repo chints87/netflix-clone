b206c95ee9a11aed5d5e87ad287a3cc3a7d3801d
chints87 <cgandhi4@gmail.com>
2022-01-11 15:06:42 +0530

    Update logo image

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -23,7 +23,7 @@ export default function Nav() {
     <div className={navBarColor.join(' ')}>
       <img
         className={styles.logo}
-        src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png"
+        src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
         alt=""
       />
       <img
