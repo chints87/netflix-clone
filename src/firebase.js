a6d715f408c9637ab8fc8aac79577e70c3e9880a
Chintan <cgandhi4@gmail.com>
2022-02-26 12:23:59 +0530

    Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,51 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
+  measurementId: process.env.REACT_APP_FIRBASE_MEASUREMENT_ID,
+};
+
+// Initialize netflix-clone app on firebase
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+// Connect to db
+const db = firebaseApp.firestore();
+
+// // Create a user in the firestore db
+// export const createUser = async (userAuth) => {
+//   // If the user is logged out then don't do anything
+//   if (!userAuth) {
+//     return null;
+//   }
+
+//   // Check if user exists in the database
+//   const userRef = db.doc(`users/${userAuth.uid}`);
+
+//   // Make a call to get user from the db
+//   const snapShot = await userRef.get();
+
+//   // If the user does not exist in the db,
+//   // then create a user
+//   if (!snapShot.exists) {
+//     const { email } = userAuth;
+//     const createdAt = new Date();
+
+//     try {
+//       await userRef.set({
+//         email,
+//         createdAt,
+//       });
+//     } catch (error) {
+//       console.log('error', error);
+//     }
+//   }
+
+//   return userRef;
+// };
+
+export default db;
